a12034a7a06f1ce9969040414eedb129373f8e19
Adis000n <zapasowyadi@gmail.com>
2024-10-06 15:31:09 +0200

    wybieranie danych od pytania w adminie

---
+++ admin/js/pytania_script.js
@@ -0,0 +1,47 @@
+var Pytanie = {
+    kategoria: '',
+    punkty: '',
+    numerDruzyny: ''
+};
+
+function updateSubmitButtonState() {
+    const submitButton = document.getElementById('submitButton');
+    if (Pytanie.kategoria && Pytanie.punkty && Pytanie.numerDruzyny) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+}
+
+function selectOption(selectedButton, typ) {
+    const buttons = selectedButton.closest('.btn-group').querySelectorAll('.btn');
+    buttons.forEach(function(button) {
+        button.classList.remove('btn-primary');
+        button.classList.add('btn-outline-primary');
+    });
+
+    selectedButton.classList.remove('btn-outline-primary');
+    selectedButton.classList.add('btn-primary');
+
+    Pytanie[typ] = selectedButton.value;
+
+    updateSubmitButtonState();
+}
+
+function submitPytanie() {
+    console.log(Pytanie);
+    const message = { dane_pytanie: Pytanie }; 
+    sendMessage(JSON.stringify(message));
+
+    const allButtons = document.querySelectorAll('.btn-group .btn');
+    allButtons.forEach(function(button) {
+        button.classList.remove('btn-primary');
+        button.classList.add('btn-outline-primary');
+    });
+
+    Pytanie.kategoria = '';
+    Pytanie.punkty = '';
+    Pytanie.numerDruzyny = '';
+
+    document.getElementById('submitButton').disabled = true;
+}
