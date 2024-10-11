abed86a23aa475a07077a214de67801b4eb9cde4
kjg6 <146731014+kjg6@users.noreply.github.com>
2024-10-11 17:09:15 +0200

    dodawanie drużyn i wysyłanie

---
+++ admin/js/druzyny_script.js
@@ -0,0 +1,18 @@
+var druzyny=[];
+function startevent() { //funkcja która działa po naciścięciu start konkursu
+      ilosc_druzyn=Number(prompt("Podaj liczbe druzyn min 2 max 4)",4)) // PROMPT do podania liczby druzyn
+      for(let i=1;i<=ilosc_druzyn;i++){                                 //Pentla od nazw drużyn
+      druzyny.push(prompt("Podaj nazwę drużyny "+ i, "Drużyna " +i))    //wprowadza dane do tablicy
+  }   
+  console.table(druzyny);
+  const message = { nazwy_druzyny: druzyny }; 
+  sendMessage(JSON.stringify(message));
+}
+function stopevent(){
+var stan = confirm("ZAKOŃCZ KONKURS");
+  if(stan==true){
+  druzyny=[];
+  const message = { nazwy_druzyny: druzyny };
+  sendMessage(JSON.stringify(message));}
+ 
+}
