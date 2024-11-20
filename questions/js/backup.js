eafcde74ac48a2593e904bf62571b5f0d1f586bb
adrian szczotka <zapasowyadi@gmail.com>
2024-11-20 09:56:47 +0100

    wyświetlanie zdjęcia pytania przy backupie

--- questions/js/backup.js
+++ questions/js/backup.js
@@ -1,13 +1,34 @@
 function backupAll(backup_data){
     switch(backup_data.stan){
         case "pytanie":
+            showPytanie_data({kategoria:backup_data.kategoria,punkty:backup_data.poziom,numerDruzyny:backup_data.nr_druzyny});
+            pytanie_img_path = backup_data.img_pytania;
+            pytanie_img.src = `../${backup_data.img_pytania}`;
 
+            pytanie_img.classList.remove("slide-in"); 
+            void pytanie_img.offsetWidth; 
+            pytanie_img.classList.add("slide-in");
+            if(backup_data.media_typ == "wideo"){
+                wideo_element.querySelector("source").src = `../${backup_data.media}`;
+                wideo_element.load();
+                wideo_element.removeAttribute("hidden");
+                wideo_element.classList.remove("slide-in"); 
+                void wideo_element.offsetWidth; 
+                wideo_element.classList.add("slide-in"); 
+            } else if(backup_data.media_typ == "audio"){
+                audio_element.querySelector("source").src = `../${backup_data.media}`;
+                audio_element.load();
+                audio_element.removeAttribute("hidden");
+                audio_element.classList.remove( "slide-in"); 
+                void audio_element.offsetWidth; 
+                audio_element.classList.add("slide-in"); 
+            }
         break;
         case "odpowiedz":
 
         break;
         case "clear":
-            
+        clearAll();
         break;
     }
 }
