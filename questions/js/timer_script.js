f103fc3b9daac0d8950ae0c13f990fb56913a265
KreceSieNaPufie <kubustomi2007@gmail.com>
2024-11-20 21:22:12 +0100

    Plansza skalowanie

--- questions/js/timer_script.js
+++ questions/js/timer_script.js
@@ -1,6 +1,9 @@
 let timerInterval;
 let currentTime = 30;
 
+
+const alarmSound = new Audio('js/bell-ring.mp3');
+
 function startTimer() {
     timerInterval = setInterval(() => {
         if (currentTime > 0) {
@@ -8,6 +11,8 @@ function startTimer() {
             document.getElementById('timer').innerText = currentTime;
         } else {
             clearInterval(timerInterval);
+            showEndMessage(); 
+            playAlarm();      
         }
     }, 1000);
 }
@@ -19,5 +24,45 @@ function stopTimer() {
 function resetTimer() {
     currentTime = 30;
     document.getElementById('timer').innerText = currentTime;
+    removeEndMessage(); 
+    stopAlarm();        
 }
 
+function showEndMessage() {
+    if (!document.getElementById('end-message')) {
+        const endMessage = document.createElement('div');
+        endMessage.id = 'end-message';
+        endMessage.innerText = 'KONIEC CZASU';
+        endMessage.style.position = 'fixed';
+        endMessage.style.top = '0';
+        endMessage.style.left = '0';
+        endMessage.style.width = '100%';
+        endMessage.style.height = '100%';
+        endMessage.style.display = 'flex';
+        endMessage.style.justifyContent = 'center';
+        endMessage.style.alignItems = 'center';
+        endMessage.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+        endMessage.style.color = 'white';
+        endMessage.style.fontSize = '3rem';
+        endMessage.style.zIndex = '1000';
+        document.body.appendChild(endMessage);
+    }
+}
+
+function removeEndMessage() {
+    const endMessage = document.getElementById('end-message');
+    if (endMessage) {
+        document.body.removeChild(endMessage);
+    }
+}
+
+
+function playAlarm() {
+    alarmSound.play();
+}
+
+
+function stopAlarm() {
+    alarmSound.pause();
+    alarmSound.currentTime = 0; 
+}
