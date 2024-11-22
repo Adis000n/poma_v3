6fa4473a7014ea72892aa9870714df37ea9b6fda
Adis000n <zapasowyadi@gmail.com>
2024-11-22 18:26:16 +0100

    poprawki żeby przyciski drużyn które nie grają się wyłączały

--- admin/js/druzyny.js
+++ admin/js/druzyny.js
@@ -1,21 +1,24 @@
 var druzyny=[];
 function startevent() { 
   druzyny=[];
-      ilosc_druzyn=Number(prompt("Podaj liczbe druzyn min 2 max 4)",4)) 
-      for(let i=1;i<=ilosc_druzyn;i++){                                 
-      druzyny.push(prompt("Podaj nazwę drużyny "+ i, "Drużyna " +i))   
-  }   
+  ilosc_druzyn=Number(prompt("Podaj liczbe druzyn min 2 max 4)",4)) 
+  for(let i=1;i<=ilosc_druzyn;i++) druzyny.push(prompt("Podaj nazwę drużyny "+ i, "Drużyna " +i));
   const message = { nazwy_druzyny: druzyny }; 
   sendMessage(JSON.stringify(message));
   const xhr3 = new XMLHttpRequest();
   xhr3.open('POST', 'http://localhost/projekty/poma_v3/admin/php/insert-nazwy-druzyny.php', true);
   xhr3.setRequestHeader('Content-Type', 'application/json');
   xhr3.onreadystatechange = function() {
-    if (xhr3.readyState === 4 && xhr3.status === 200) {
-      console.log(xhr3.responseText);
-    }
+    if (xhr3.readyState === 4 && xhr3.status === 200) console.log(xhr3.responseText);
   };
   xhr3.send(JSON.stringify(message));
+
+  ['team3', 'team4'].forEach((id, index) => {
+    const button = document.getElementById(id);
+    const disabled = ilosc_druzyn <= index + 2;
+    button.disabled = disabled;
+    button.classList.toggle('disabled', disabled);
+  });
 }
 
 
