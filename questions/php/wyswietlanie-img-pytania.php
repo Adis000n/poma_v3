41a64bea8579806f2ffb7cba13c86c54590db330
Adis000n <zapasowyadi@gmail.com>
2025-01-09 21:12:43 +0100

    logowanie id pytań do tabeli w bazie

--- questions/php/wyswietlanie-img-pytania.php
+++ questions/php/wyswietlanie-img-pytania.php
@@ -14,10 +14,12 @@ if (isset($_GET['kategoria']) && isset($_GET['punkty'])&& isset($_GET['nr_druzyn
 }
 
 function getRandomImage($kategoria, $punkty,$conn) {
-    $result = mysqli_query($conn, "SELECT img_pytania, media,media_typ FROM mvc_konkurs_pytania WHERE kategoria='$kategoria' AND poziom=$punkty AND (YEAR(CURDATE())-rok_uzycia)>=5 ORDER BY RAND() LIMIT 1;");
+    $result = mysqli_query($conn, "SELECT id, img_pytania, media,media_typ FROM mvc_konkurs_pytania WHERE kategoria='$kategoria' AND poziom=$punkty AND (YEAR(CURDATE())-rok_uzycia)>=5 ORDER BY RAND() LIMIT 1;");
     if ($result && mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_array($result);
         $imagePath = $row['img_pytania'];
+        $id = $row['id'];
+        logId($id,$conn);
         $media = $row['media'];
         $media_typ = $row['media_typ'];
         return array("$imagePath", "$media_typ","$media");
@@ -51,6 +53,13 @@ function SetRokUzycia($pytanie_path,$conn){
         die("Error updating record: " . mysqli_stmt_error($update_stmt));
     }
     mysqli_stmt_close($update_stmt);
+}
+
+function logId($id,$conn){
+    $update_query = "INSERT INTO `id_pytan_logs` (id_pytania,time_pin) VALUES ($id,now())";
+    if (!mysqli_query($conn, $update_query)) {
+        die("Error inserting record: " . mysqli_error($conn));
+    }
 }
 
 ?>
