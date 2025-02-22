69d8f5d90c0fc7a0d88d5deadb7c285c9247c311
Adis000n <zapasowyadi@gmail.com>
2025-02-22 17:42:15 +0100

    Dodane dzwieki do bonusow

--- questions/js/kolo.js
+++ questions/js/kolo.js
@@ -1,15 +1,20 @@
  // Trzeba wysyłać wartości true / false (true - pokarze, false - zamknie)
+ var powerup = new Audio('js/audio_powerup.mp3');
+ var powerup2 = new Audio('js/audio_powerup2.mp3');
  function showBoostersWheele() {
+    powerup.play();
     const overlay = document.getElementById('overlay');
     overlay.innerHTML = `<div class="overlay-background"><div class="overlay-content1">Losowanie<br>Bonusu</div></div>`;
     toggleOverlay(true)
 }
 function toggleOverlay(show) {
+    console.log("backup moment");
     const overlay = document.getElementById('overlay');
     overlay.style.display = show ? 'flex' : 'none';
 }
 // Trzeba wysyłać druzynę (Numer drużyny), booster który został wylosowany
 function pokazBooster(booster) {
+    powerup2.play();
     const overlay = document.getElementById('overlay');
     overlay.innerHTML = `<div class="overlay-content">Otrzymano bonus:<br> ${booster} <br>`;
     setTimeout(function () {
