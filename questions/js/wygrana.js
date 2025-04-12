2f354bec46a8eacdac2d5279dce6b115cafa1059
Adis000n <zapasowyadi@gmail.com>
2025-04-12 21:34:48 +0200

    double check na przycisku od wygrywania

--- questions/js/wygrana.js
+++ questions/js/wygrana.js
@@ -1,3 +1,5 @@
+const trabkaAudio = new Audio('js/trabka.mp3');
+trabkaAudio.volume = 1;
 function displayWinningTeam(message) {
     
     const overlay = document.createElement('div');
@@ -52,8 +54,8 @@ function displayWinningTeam(message) {
     overlay.appendChild(confettiGif);
     document.body.appendChild(overlay);
 
-    const audioElement = new Audio('js/trabka.mp3');
-    audioElement.play().catch(error => console.log("Autoodtwarzanie zablokowane:", error));
+
+    trabkaAudio.play();
 
     setTimeout(() => {
         overlay.style.display = 'none';  
