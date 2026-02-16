04eea5e2bcf445295116bba1e6767a35968372da
Lolon <pawlogawlo2007@gmail.com>
2026-02-16 10:04:54 +0100

    path to poma

--- admin/js/setup_variables.js
+++ admin/js/setup_variables.js
@@ -1,5 +1,5 @@
 // domyślne ustawienia
-PATH_TO_POMA = 'http://localhost/projekty/poma_v3'; //ścieżka do folderu przez localhosta
+PATH_TO_POMA = 'http://localhost/poma_v3'; //ścieżka do folderu przez localhosta
 USE_SWEETALERT = true; // czy używać customowego wyglądu pry zaczynaniu konkursu
 DISABLE_TEAMS = true; // czy wyłączać przyciski dla drużyn które nie grają (przy wybieraniu pytania, ręcznego ustawiania punktów)
 POINTS_AUTOMAT = true; //czy punkty dodają się automatycznie po kliknięciu że poprawna odpowiedź czy trzeba manulanie zmienić
